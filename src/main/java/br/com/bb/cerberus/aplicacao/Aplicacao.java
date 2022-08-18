7dd069e69b8ebf803752a2746eaa76b759162f4f
gbragabb <108825832+gbragabb@users.noreply.github.com>
2022-08-18 14:29:55 -0300

    Implementação de leitura e escrita paralela
    Aplicação de multi-thread para processamento e saída de arquivos.

--- src/main/java/br/com/bb/cerberus/aplicacao/Aplicacao.java
+++ src/main/java/br/com/bb/cerberus/aplicacao/Aplicacao.java
@@ -12,11 +12,13 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,28 +29,56 @@ public class Aplicacao {
 	private static final String CAMINHO_ABSOLUTO = System.getProperty("user.dir") + "/src/main/java/";
 	private static final Charset UTF8 = Charset.forName("UTF-8");
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		List<String> tempos = new ArrayList<String>();
 		Instant ini = Instant.now();
-		List<Movie> movies = new ArrayList<>();
+		List<Movie> movies = Collections.synchronizedList(new ArrayList<>());
+		CountDownLatch cdl = new CountDownLatch(3);
 
 		//TODO -> Criar as threads
 		// Usando o NIO e apenas streams
 		Path p = Paths.get(CAMINHO_ABSOLUTO + "movies1.csv");
-
-		try {
-			Files.lines(p, UTF8).skip(1).map(l -> l.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)).forEach(arr -> {
-				movies.add(new Movie(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9],
-						arr[10], arr[11]));
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		// Lista dos filmes de terror
-		gerarArquivo("Filmes de Terror", movies.stream().filter(m -> m.isOfGenre("Horror"))
-				.sorted(Comparator.comparing(Movie::getRating).reversed()).limit(20).collect(Collectors.toList()));
-
+		Path p2 = Paths.get(CAMINHO_ABSOLUTO + "movies2.csv");
+		Path p3 = Paths.get(CAMINHO_ABSOLUTO + "movies3.csv");
+		Thread t1 = new Thread(() -> {
+			try {
+				Files.lines(p, UTF8).skip(1).map(l -> l.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)).forEach(arr -> {
+					movies.add(new Movie(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9],
+							arr[10], arr[11]));
+				});
+			} catch (Exception e) {
+				System.out.println("Erro");
+			}
+			cdl.countDown();
+		} );
+		Thread t2 = new Thread(() -> {
+			try {
+				Files.lines(p2, UTF8).skip(0).map(l -> l.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)).forEach(arr -> {
+					movies.add(new Movie(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9],
+							arr[10], arr[11]));
+				});
+			} catch (Exception e) {
+				System.out.println("Erro");
+			}
+			cdl.countDown();
+		} );
+		Thread t3 = new Thread(() -> {
+			try {
+				Files.lines(p3, UTF8).skip(0).map(l -> l.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)).forEach(arr -> {
+					movies.add(new Movie(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9],
+							arr[10], arr[11]));
+				});
+			} catch (Exception e) {
+				System.out.println("Erro");
+			}
+			cdl.countDown();
+		} );
+		t1.start();
+		t2.start();
+		t3.start();
+		cdl.await();
+		
+		System.out.println(movies.size());
 		// Lista dos 50 melhores por ano
 		Map<String, List<Movie>> mapAnoFilme = new HashMap<>();
 		movies.stream().map(m -> m.getYear()).distinct()
@@ -57,33 +87,32 @@ public class Aplicacao {
 								.sorted(Comparator.comparing(Movie::getRating).reversed()).limit(50)
 								.collect(Collectors.toList())));
 		// Iterando no mapa
-		mapAnoFilme.forEach((ano, lista) -> {
+		CountDownLatch cdl2 = new CountDownLatch(mapAnoFilme.size()+1);
+		List<Thread> threads = new ArrayList<Thread>();
+		threads.add(new Thread(() -> {
 			try {
-				gerarArquivo(ano, lista);
-			} catch (RuntimeException | IOException e) {
-				e.printStackTrace();
+				gerarArquivo("Filmes de Terror", movies.stream().filter(m -> m.isOfGenre("Horror"))
+				.sorted(Comparator.comparing(Movie::getRating).reversed()).limit(20).collect(Collectors.toList()));
+				cdl2.countDown();	
+			} catch (Exception e) {
+				System.out.println("erro");
 			}
+			
+		}));
+		mapAnoFilme.forEach((ano, lista) -> {
+				threads.add(new Thread(() -> {
+					try {
+						gerarArquivo(ano, lista);
+					cdl2.countDown();	
+					} catch (Exception e) {
+						System.out.println("erro");
+					}
+					
+				}));
 		});
-
-		// String filePathIn = "C:\\Users\\F8295739\\Documents\\02 -
-		// Projetos\\ProjetoArquivos\\src\\main\\java\\";
-
-		Scanner scannerIn = new Scanner(new File(CAMINHO_ABSOLUTO + "movies1.csv"));
-
-		scannerIn.nextLine();
-		while (scannerIn.hasNext()) {
-			String[] line = scannerIn.nextLine().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-			movies.add(new Movie(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8],
-					line[9], line[10], line[11]));
-		}
-
-		movies.sort(Comparator.comparing(Movie::getRating).reversed());
-		createFileMovies("reversed.csv", movies);
-
-		ArrayList<Movie> bestHorror = (ArrayList<Movie>) movies.stream().filter(Movie::isHorrorMovie).limit(20)
-				.collect(Collectors.toList());
-		createFileMovies("bestHorror.csv", bestHorror);
-
+		threads.forEach(Thread::start);
+		cdl2.await();
+		
 		Instant fim = Instant.now();
 		Duration delta = Duration.between(ini, fim);
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss.SS")
@@ -97,22 +126,6 @@ public class Aplicacao {
 		System.out.println("Tempo em milisegundos: " + delta.toMillis() + " milisegundos");
 		System.out.println("Tempo em segundos: " + delta.toSeconds() + " segundos");
 		gerarArquivo("Tempo de Processamento", tempos);
-	}
-
-	private static void createFileMovies(String fileNameOut, List<Movie> movies) throws RuntimeException, IOException {
-
-		// TODO -> automatizar o filepath
-		String filePathOut = "C:\\Users\\F8295739\\Documents\\02 - Projetos\\ProjetoArquivos\\src\\main\\java\\";
-
-		try (FileOutputStream out = new FileOutputStream(CAMINHO_ABSOLUTO.concat(fileNameOut))) {
-			movies.forEach(movie -> {
-				try {
-					out.write(movie.toString().getBytes());
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			});
-		}
 	}
 
 	// Buffered Writer é aparentemente mais rápido.
